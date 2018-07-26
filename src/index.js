c684d8e53ded60b856fb9e579d2a0994f868c216
moskalenkoBV <b.v.moskalenko@gmail.com>
2018-07-26 16:43:37 +0300

    Config deploy

--- src/index.js
+++ src/index.js
@@ -43,4 +43,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(3002, () => { console.log('Back is up on 3002') })
+app.listen(process.env.PORT || 4000, () => { console.log('Listening...') })
+
+export default app
